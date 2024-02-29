4adba6d20af28addd0bf28af4a8ad5c774ac48f9
Akanshu <122857774+Akanshu06@users.noreply.github.com>
2024-02-29 14:10:04 +0530

    routes

--- routes.js
+++ routes.js
@@ -1,15 +1,19 @@
-const http = require("http");
+///const http = require("http");
+const bodyParser =require('body-parser');
 const express = require('express');
 const app= express();
-app.use((req ,res ,next)=>{
-    console.log('in the middleware');
-    next();
+
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use('/add-product',(req ,res ,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><br><input type="number" name="size"><button type="submit">Add Product</button></form>');
 });
-app.use((req ,res ,next)=>{
-    console.log('another the middleware');
-    
-    res.send('<h1>Hello from Express</h1>')
+app.post('/product',(req ,res ,next)=>{
+    console.log(req.body); 
+    res.redirect('/'); 
 });
-const server = http.createServer(app);
-server.listen(3000);
+app.use('/',(req ,res ,next)=>{
+    res.send('<h1>Hello from Express</h1>');
+});
+app.listen(3000);
   
